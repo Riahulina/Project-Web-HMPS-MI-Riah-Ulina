85c59101b482ca1ea71188094125d593e5de1194
Riahulina <riahulinahutasoit@gmail.com>
2025-08-11 23:17:56 +0700

    Ubah Home.blade.php, hapus Welcome.blade.php, update route

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\DivisionMemberController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('Welcome');
+    return view('Home');
 });
 
 Route::get('/about', function () {
